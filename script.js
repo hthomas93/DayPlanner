b13e861b2f28a1f8a8691a7548ee602ea49064d0
Harrison Thomas <hthomas146@utexas.edu>
2019-12-20 16:52:24 -0600

    Final program. Input fields now change color based on time of day.

--- script.js
+++ script.js
@@ -7,41 +7,59 @@ $(document).ready(function () {
     console.log(displayDate);
     $(displayDate).text("Today is " + moment().format('LL'));
 
-    var storedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
-
-    var nineInput = $("#nine-input");
-    $(nineInput).attr("placeholder", (storedTodos[0] || "9am"));
-    var tenInput = $("#ten-input");
-    $(tenInput).attr("placeholder", (storedTodos[1] || "10am"));
-    var elevenInput = $("#eleven-input");
-    $(elevenInput).attr("placeholder", (storedTodos[2] || "11am"));
-
-    function storeTodos() {
-        localStorage.setItem("todos", JSON.stringify(storedTodos));
-    }
 
 
     function showTimes() {
         //dynamically add html
+        // for each number in the timesArray array
         timesArray.forEach(number => {
+            // set todoText to the value at number of localStorage, or nothing
             var todoText = localStorage.getItem(number) || "";
+            // set the HTML here
             html = `<div class="row">
         <div class="col-md-1 date">
             <p id="eleven">${number}</p>
         </div>
-        <input class="form-control form-control-lg col-md-10" type="text" id="input-${number}" value=${todoText}>
+        <input class="form-control form-control-lg col-md-10  ${number}" type="text" id="input-${number}" value=${todoText}>
 
         <button class="col-md-1 btn btn-success far fa-save time-save" data-id=${number}></button>
 
         </div>`
 
             $("#timeslots").append(html);
+
+
         })
+    }
+    showTimes();
 
+    function checkTime() {
+        timesArray.forEach(number => {
+            var hour = parseInt(moment().format('H'));
+            console.log(hour)
+            console.log(hour, number);
+            if (hour === number) {
+                $(`#input-${number}`).attr("style", "background-color:pink");
+                console.log("it's working!")
+            } else if (hour > number) {
+                console.log("it's later!")
+                $(`#input-${number}`).attr("style", "background-color:darkgrey");
+            } else if (hour === 0) {
+                clearTasks();
+            }
 
+        });
     }
 
-    showTimes();
+    function clearTasks() {
+        timesArray.forEach(number => {
+            localStorage.setItem(number, "");
+        })
+    }
+
+    checkTime();
+
+
 });
 
 $('#timeslots').on('click', '.time-save', function (event) {
@@ -51,14 +69,12 @@ $('#timeslots').on('click', '.time-save', function (event) {
     var value = $(`#input-${i}`).val()
     //store in local storage
     localStorage.setItem(i, value);
-
-    // storedTodos[i];
-    // console.log(storedTodos);
-    // storeTodos();
-    //$(`input-${i}`).attr("placeholder", storedTodos[2]);
 })
 
 
-var timesArray = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+var timesArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+
+
 
 
