a0ec00ecb1208cee923443fd20c3007c67dd73cf
Harrison Thomas <hthomas146@utexas.edu>
2019-12-19 20:42:53 -0600

    Input now saves as placeholder

--- script.js
+++ script.js
@@ -7,28 +7,55 @@ $(document).ready(function () {
     console.log(displayDate);
     $(displayDate).text("Today is " + moment().format('LL'));
 
-    var storedTodos = []
+    var storedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
 
-    $("#save-nine").on("click", function (event) {
-        event.preventDefault();
-        console.log("It works")
-        storedTodos.push(["9am", $("#nine-input").val()]);
-        console.log(storedTodos);
-        storeTodos();
-
-    })
+    var nineInput = $("#nine-input");
+    $(nineInput).attr("placeholder", (storedTodos[0] || "9am"));
+    var tenInput = $("#ten-input");
+    $(tenInput).attr("placeholder", (storedTodos[1] || "10am"));
+    var elevenInput = $("#eleven-input");
+    $(elevenInput).attr("placeholder", (storedTodos[2] || "11am"));
 
     function storeTodos() {
         localStorage.setItem("todos", JSON.stringify(storedTodos));
     }
 
-    // when the save-nine button is clicked
-    // push the text in nine-input to the storedTodos array
-    //return the storedTodos array
+
+    $("#save-nine").on("click", function (event) {
+        event.preventDefault();
+        console.log("It works")
+        storedTodos[0] = $(nineInput).val();
+        console.log(storedTodos);
+        storeTodos();
+        $(nineInput).attr("placeholder", storedTodos[0]);
+    })
+
+    $("#save-ten").on("click", function (event) {
+        event.preventDefault();
+        console.log("It works")
+        storedTodos[1] = $(tenInput).val();
+        console.log(storedTodos);
+        storeTodos();
+        $(tenInput).attr("placeholder", storedTodos[1]);
+    })
+
+    $("#save-eleven").on("click", function (event) {
+        event.preventDefault();
+        console.log("It works")
+        storedTodos[2] = $(elevenInput).val();
+        console.log(storedTodos);
+        storeTodos();
+        $(elevenInput).attr("placeholder", storedTodos[2]);
+    })
+
 
 });
+
+
 
 // don't forget preventDefault()
 // when you click on the adjacent save button
 // the value of the form (with appropriate ID) is stored along with the time slot to storedTodos
 // 
+
+// function
